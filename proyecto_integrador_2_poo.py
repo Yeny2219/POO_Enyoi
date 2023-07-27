217a095b00ab1e7b3b8f8c9ed7a3f9fdee6eeac0
Yeny2219 <arq.yagonzalezu@gmail.com>
2023-07-27 12:15:56 -0500

    POO clases con atributos

---
+++ proyecto_integrador_2_poo.py
@@ -0,0 +1,93 @@
+class Personas:
+    nombre=""
+    apellidos=""
+    edad=0
+    cedula=""
+
+class Animales:
+    nombre=""
+    genero=""
+    raza=""
+
+class Veterinaria:
+    nombre=""
+    direccion=""
+    telefono=""
+
+
+
+
+persona1 = Personas()
+
+persona1.nombre = "Yeny"
+persona1.apellidos = "González"
+persona1.edad = 30
+persona1.cedula = "1234567890"
+
+print("Primera clase: Personas")
+print("Nombre:", persona1.nombre)
+print("Apellido:", persona1.apellidos)
+print("Edad:", persona1.edad)
+print("Cédula:", persona1.cedula)
+
+
+animales1 = Animales()
+
+animales1.nombre="Fargo"
+animales1.genero="macho"
+animales1.raza ="Husky Siberiano"
+
+print("Segunda clase: Animales")
+print("Nombre:", animales1.nombre)
+print("Genero", animales1.genero )
+print("Raza:", animales1.raza)
+
+
+veterinaria1 = Veterinaria()
+
+veterinaria1.nombre = "Riovet"
+veterinaria1.direccion="Avenida 7 de agosto, Riosucio"
+veterinaria1.telefono="(+57)-3023568799"
+
+print("Tercera clase: Veterinaria")
+print("Nombre:", veterinaria1.nombre)
+print("Dirección:", veterinaria1.direccion)
+print("Teléfono:", veterinaria1.telefono)
+
+
+persona1 = Personas()
+
+persona1.nombre = "Olga"
+persona1.apellidos = "Uribe"
+persona1.edad = 40 
+persona1.cedula = "30526413"
+
+print("Primera clase: Personas")
+print("Nombre:", persona1.nombre)
+print("Apellido:", persona1.apellidos)
+print("Edad:", persona1.edad)
+print("Cédula:", persona1.cedula)
+
+
+animales1 = Animales()
+
+animales1.nombre="Valhalla"
+animales1.genero="macho"
+animales1.raza ="Bombay"
+
+print("Segunda clase: Animales")
+print("Nombre:", animales1.nombre)
+print("Genero", animales1.genero )
+print("Raza:", animales1.raza)
+
+
+veterinaria1 = Veterinaria()
+
+veterinaria1.nombre = "Zooplanet"
+veterinaria1.direccion="Carrera 5 #45-09, Riosucio"
+veterinaria1.telefono="(+57)-3217423685"
+
+print("Tercera clase: Veterinaria")
+print("Nombre:", veterinaria1.nombre)
+print("Dirección:", veterinaria1.direccion)
+print("Teléfono:", veterinaria1.telefono)
