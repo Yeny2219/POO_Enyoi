bb553671c99b249ebac30b01a20b9ee326b3fbc5
Yeny2219 <arq.yagonzalezu@gmail.com>
2023-07-27 14:23:26 -0500

    Se adiciona datos que no sobreescriben los 1°s

--- proyecto_integrador_2_poo.py
+++ proyecto_integrador_2_poo.py
@@ -55,41 +55,41 @@ print("Dirección:", veterinaria1.direccion)
 print("Teléfono:", veterinaria1.telefono)
 
 
-persona1 = Personas()
+persona2 = Personas()
 
-persona1.nombre = "N.A"
-persona1.apellidos = "N.A"
-persona1.edad = "N.A" 
-persona1.cedula = "N.A"
+persona2.nombre = "N.A"
+persona2.apellidos = "N.A"
+persona2.edad = "N.A" 
+persona2.cedula = "N.A"
 
 print("Primera clase: Personas")
-print("Nombre:", persona1.nombre)
-print("Apellido:", persona1.apellidos)
-print("Edad:", persona1.edad)
-print("Cédula:", persona1.cedula)
+print("Nombre:", persona2.nombre)
+print("Apellido:", persona2.apellidos)
+print("Edad:", persona2.edad)
+print("Cédula:", persona2.cedula)
 
 
-animales1 = Animales()
+animales2 = Animales()
 
-animales1.nombre="Lula"
-animales1.genero="Hembra"
-animales1.raza ="Gata-siames"
+animales2.nombre="Lula"
+animales2.genero="Hembra"
+animales2.raza ="Gata-siames"
 
 print("Segunda clase: Animales")
-print("Nombre:", animales1.nombre)
-print("Genero", animales1.genero )
-print("Raza:", animales1.raza)
+print("Nombre:", animales2.nombre)
+print("Genero", animales2.genero )
+print("Raza:", animales2.raza)
 
 
-veterinaria1 = Veterinaria()
+veterinaria2 = Veterinaria()
 
-veterinaria1.nombre = "Zooplanet"
-veterinaria1.direccion="Carrera 5 #45-09, Riosucio"
-veterinaria1.telefono="(+57)-3217423685"
+veterinaria2.nombre = "Zooplanet"
+veterinaria2.direccion="Carrera 5 #45-09, Riosucio"
+veterinaria2.telefono="(+57)-3217423685"
 
 print("Tercera clase: Veterinaria")
-print("Nombre:", veterinaria1.nombre)
-print("Dirección:", veterinaria1.direccion)
-print("Teléfono:", veterinaria1.telefono)
+print("Nombre:", veterinaria2.nombre)
+print("Dirección:", veterinaria2.direccion)
+print("Teléfono:", veterinaria2.telefono)
 
 
