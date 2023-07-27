7c43499bf4623f5ba1eabd82d73fb0575f40e953
Yeny2219 <140571789+Yeny2219@users.noreply.github.com>
2023-07-27 13:48:06 -0500

    Se creo clases.js
    Diagrama de clases creado en un lenguaje diferente

---
+++ clases.js
@@ -0,0 +1,41 @@
+{
+    "Personas" [
+      {
+        "nombre": "Yeny",
+        "apellidos": "González",
+        "edad": 30,
+        "cedula": "1234567890"
+      },
+      {
+        "nombre": "Olga",
+        "apellidos": "Uribe",
+        "edad": 40,
+        "cedula": "30526413"
+      }
+    ],
+    "Animales" [
+      {
+        "nombre": "Fargo",
+        "genero": "macho",
+        "raza": "Husky Siberiano"
+      },
+      {
+        "nombre": "Valhalla",
+        "genero": "macho",
+        "raza": "Bombay"
+      }
+    ],
+    "Veterinaria" [
+      {
+        "nombre": "Riovet",
+        "direccion": "Avenida 7 de agosto, Riosucio",
+        "telefono": "(+57)-3023218479"
+      },
+      {
+        "nombre": "Zooplanet",
+        "direccion": "Carrera 5 #45-09, Riosucio",
+        "telefono": "(+57)-3217423685"
+      }
+    ]
+  }
+  
